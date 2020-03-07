8bde6e667745876553b4cba65ee676a04008948b
Jeff Greco <jeffpgreco@gmail.com>
2020-03-07 02:51:51 -0500

    connected mongo database

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import API from "./utils/API";
+
+
+class App extends Component {
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    let testStudent = {
+      name: "dude",
+      studentId: "hdhd76ehf5"
+    }
+
+    API.saveStudent(testStudent)
+            .then(res => alert("student added!"))
+            .catch(err => console.log(err))
+  };
+
+  render() {
+    return (
+      <div className="App">
+       <button onClick={this.handleFormSubmit}>Test</button>
+      </div>
+    );
+  }
 }
 
 export default App;
